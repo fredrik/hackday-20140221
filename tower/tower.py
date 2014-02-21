2a512770014f15c8d66ec5c7e29c911451cc98bf
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2014-02-21 17:31:17 +0000

    split out Worker

--- tower/tower.py
+++ tower/tower.py
@@ -3,13 +3,11 @@
 """
 
 import json
-import uuid
-import md5
-from time import time
 
 from flask import Flask
 from flask import request
 
+from worker import Worker
 
 app = Flask('control')
 
@@ -19,25 +17,6 @@ app = Flask('control')
 databank = {
     'workers': {},
 }
-
-
-class Worker():
-    """Represent a worker."""
-    def __init__(self, *args, **kwargs):
-        self.type = kwargs.get('type')
-        self.address = kwargs.get('address')
-        self.id = str(uuid.uuid4())
-        self.secret_key = self._generate_secret_key()
-
-    def _generate_secret_key(self):
-        return md5.new(str(time())).hexdigest()
-
-    def as_dict(self):
-        return {
-            'id': self.id,
-            'type': self.type,
-            'address': self.address,
-        }
 
 
 # TODO:
