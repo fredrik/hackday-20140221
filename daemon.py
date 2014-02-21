ef53b049fd7192f7e17c229b1465a6eceb0fd675
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2014-02-21 11:21:05 +0000

    serve status over http

--- daemon.py
+++ daemon.py
@@ -1,6 +1,10 @@
 import logging
 
 import gevent
+from gevent.wsgi import WSGIServer
+
+from app import app
+
 
 logger = logging.getLogger('daemon')
 
@@ -11,8 +15,6 @@ class SomeDaemon(object):
 
     def start(self):
         """Boot workers."""
-        # one 'run' worker,
-        # one status worker.
         worker = gevent.spawn(self.run)
         status = gevent.spawn(self.status)
 
@@ -25,9 +27,10 @@ class SomeDaemon(object):
             gevent.sleep(2)
 
     def status(self):
-        while True:
-            print 'all good.'
-            gevent.sleep(2.5)
+        logger.info('status worker: starting.')
+        http_server = WSGIServer(('', 5000), app)
+        http_server.serve_forever()
+        logger.info('status worker is done/dead.')
 
     def run(self):
         while True:
