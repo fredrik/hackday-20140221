38a64ceec6c01b1e461f08a247f99f6b4e86474f
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2014-02-21 12:05:46 +0000

    _status return version, uptime and stats

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-
-
 @app.route('/')
 def status():
-    return json.dumps(app._status)
+    try:
+        status = app._status()
+    except Exception, e:
+        print e.__class__, e
+        return json.dumps({'status': 'fail'})
+
+    return json.dumps(status)
--- daemon.py
+++ daemon.py
@@ -1,3 +1,6 @@
+from time import time
+from collections import Counter
+
 import gevent
 from gevent.wsgi import WSGIServer
 
@@ -5,37 +8,46 @@ from app import app
 
 
 class SomeDaemon(object):
+    VERSION = '0.1'
+
     def __init__(self):
-        self._status = {
+        self._started_at = time()
+        self._stats = Counter()
+
+    def _status(self):
+        """Returns a status dict."""
+        return {
             'status': 'ok',
-            'units': 0,
+            'version': self.VERSION,
+            'uptime': int(time() - self._started_at),  # seconds
+            'stats': self._stats,
         }
 
-    def start(self):
+    def run(self):
         """Boot workers."""
-        worker = gevent.spawn(self.run)
-        status = gevent.spawn(self.status)
+        worker = gevent.spawn(self.worker)
+        status = gevent.spawn(self.status_worker)
 
         worker.join()
         print 'worker done:', worker.get()
         status.kill()
         print 'done.'
 
-    def status(self):
+    def status_worker(self):
         app._status = self._status
         http_server = WSGIServer(('', 5000), app)
         http_server.serve_forever()
 
-    def run(self):
-        for i in range(5):
-            print 'running'
-            self._status['units'] += 1
+    def worker(self):
+        while True:
+            print 'work work'
+            self._stats['units'] += 1
             gevent.sleep(0.25)
         return 32
 
 
 def main():
-    SomeDaemon().start()
+    SomeDaemon().run()
 
 
 if __name__ == '__main__':
