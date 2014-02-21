3cea1eed3a9b3067659b419b3055ddb8c6a5c181
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2014-02-21 11:08:08 +0000

    spawn workers

--- daemon.py
+++ daemon.py
@@ -1,5 +1,6 @@
-import time
 import logging
+
+import gevent
 
 logger = logging.getLogger('daemon')
 
@@ -8,14 +9,34 @@ class SomeDaemon(object):
     def __init__(self):
         logger.info('SomeDaemon starting.')
 
+    def start(self):
+        """Boot workers."""
+        # one 'run' worker,
+        # one status worker.
+        worker = gevent.spawn(self.run)
+        status = gevent.spawn(self.status)
+
+        while True:
+            if worker.ready():
+                print 'worker done.'
+            if status.ready():
+                print 'status done.'
+            print 'zzz'
+            gevent.sleep(2)
+
+    def status(self):
+        while True:
+            print 'all good.'
+            gevent.sleep(2.5)
+
     def run(self):
         while True:
             print 'running'
-            time.sleep(0.25)
+            gevent.sleep(0.25)
 
 
 def main():
-    SomeDaemon().run()
+    SomeDaemon().start()
 
 
 if __name__ == '__main__':
