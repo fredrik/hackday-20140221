2f09271618c561676261e2a00ecba9889e88a9ad
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2014-03-08 23:55:08 +0000

    worker: use sha
    and move id to a more pleasing location

--- tower/worker.py
+++ tower/worker.py
@@ -1,18 +1,18 @@
+import sha
 import uuid
-import md5
 from time import time
 
 
 class Worker():
     """Represent a worker."""
     def __init__(self, *args, **kwargs):
+        self.id = str(uuid.uuid4())
         self.type = kwargs.get('type')
         self.address = kwargs.get('address')
-        self.id = str(uuid.uuid4())
         self.secret_key = self._generate_secret_key()
 
     def _generate_secret_key(self):
-        return md5.new(str(time())).hexdigest()
+        return sha.new(str(time())).hexdigest()
 
     def as_dict(self):
         return {
